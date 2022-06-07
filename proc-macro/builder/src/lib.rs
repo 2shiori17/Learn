75764ef5392cc2ee4b3e4325e9a491240e1f9bed
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2022-06-07 11:19:49 +0900

    proc-macro: builder/02-create-builder

--- proc-macro/builder/src/lib.rs
+++ proc-macro/builder/src/lib.rs
@@ -1,17 +1,56 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
-use quote::quote;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    let ident = input.ident;
-    let expanded = quote! {
-        impl #ident {
-            pub fn hello() {
-                println!("Hello, world!");
+    match input.data {
+        Data::Struct(ref data) => struct_builder(&input, data),
+        _ => unimplemented!(),
+    }
+}
+
+fn struct_builder(input: &DeriveInput, data: &DataStruct) -> TokenStream {
+    let target = &input.ident;
+    let builder = format_ident!("{}Builder", target);
+    let partial = partial(&data.fields);
+    let init = init(&data.fields);
+    TokenStream::from(quote! {
+        impl #target {
+            pub fn builder() -> #builder {
+                #builder {
+                    #init
+                }
             }
         }
-    };
-    TokenStream::from(expanded)
+
+        pub struct #builder {
+            #partial
+        }
+    })
+}
+
+fn partial(fields: &Fields) -> TokenStream2 {
+    fields
+        .iter()
+        .map(|field| {
+            let ty = &field.ty;
+            let ident = &field.ident;
+            quote! { #ident: std::option::Option<#ty>, }
+        })
+        .flatten()
+        .collect()
+}
+
+fn init(fields: &Fields) -> TokenStream2 {
+    fields
+        .iter()
+        .map(|field| {
+            let ident = &field.ident;
+            quote! { #ident: None, }
+        })
+        .flatten()
+        .collect()
 }
