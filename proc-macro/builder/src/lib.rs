66c2b31253e9e6b5914de1e065b344afbbd83382
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2022-06-07 11:16:28 +0900

    proc-macro: builder/01-parse

--- proc-macro/builder/src/lib.rs
+++ proc-macro/builder/src/lib.rs
@@ -1,8 +1,17 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    let expanded = quote! {
+        impl #ident {
+            pub fn hello() {
+                println!("Hello, world!");
+            }
+        }
+    };
+    TokenStream::from(expanded)
 }
