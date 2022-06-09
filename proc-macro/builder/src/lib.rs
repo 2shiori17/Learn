f0fde9210cc0ad3ea5c9b3b3076525387182b919
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2022-06-09 18:31:37 +0900

    proc-macro: builder/06-optional-field

--- proc-macro/builder/src/lib.rs
+++ proc-macro/builder/src/lib.rs
@@ -1,7 +1,10 @@
 use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, Ident};
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, GenericArgument, Ident,
+    PathArguments, PathSegment, Type,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -55,7 +58,7 @@ where
 
 fn partial(fields: &Fields) -> TokenStream2 {
     fields_map(fields, |field| {
-        let ty = &field.ty;
+        let ty = unwrap_option(&field.ty);
         let ident = &field.ident;
         quote! { #ident: std::option::Option<#ty>, }
     })
@@ -70,7 +73,7 @@ fn init(fields: &Fields) -> TokenStream2 {
 
 fn setters(fields: &Fields) -> TokenStream2 {
     fields_map(fields, |field| {
-        let ty = &field.ty;
+        let ty = unwrap_option(&field.ty);
         let ident = &field.ident;
         quote! {
             pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
@@ -82,32 +85,66 @@ fn setters(fields: &Fields) -> TokenStream2 {
 }
 
 fn build(target: &Ident, fields: &Fields) -> TokenStream2 {
+    let unwrapped = fields_map(fields, |field| {
+        let ident = &field.ident;
+        let err_msg = format!("{:?} is required", &ident);
+        if is_option(&field.ty) {
+            quote! {
+                let #ident = self.#ident.clone();
+            }
+        } else {
+            quote! {
+                let #ident = if let Some(x) = &self.#ident {
+                    x.clone()
+                } else {
+                    return Err(#err_msg.into());
+                };
+            }
+        }
+    });
+
     let idents = fields_map(fields, |field| {
         let ident = &field.ident;
         quote! { #ident, }
     });
 
-    let self_idents = fields_map(fields, |field| {
-        let ident = &field.ident;
-        quote! { &self.#ident, }
-    });
-
-    let some_idents = fields_map(fields, |field| {
-        let ident = &field.ident;
-        quote! { Some(#ident), }
-    });
-
-    let clone_idents = fields_map(fields, |field| {
-        let ident = &field.ident;
-        quote! { #ident.clone(), }
-    });
-
     quote! {
-        let (#idents) = match (#self_idents) {
-            (#some_idents) => (#clone_idents),
-            _ => return Err("not set".into()),
-        };
-
+        #unwrapped
         Ok(#target { #idents })
+    }
+}
+
+fn is_option(ty: &Type) -> bool {
+    first_path_segment(ty)
+        .map(|seg| seg.ident == "Option")
+        .unwrap_or(false)
+}
+
+fn unwrap_option(ty: &Type) -> &Type {
+    first_path_segment(ty)
+        .and_then(|seg| (seg.ident == "Option").then(|| &seg.arguments))
+        .and_then(|args| first_generic_argument(args))
+        .and_then(|arg| get_generic_type(arg))
+        .unwrap_or(ty)
+}
+
+fn first_path_segment(ty: &Type) -> Option<&PathSegment> {
+    match ty {
+        Type::Path(path) => path.path.segments.first(),
+        _ => None,
+    }
+}
+
+fn first_generic_argument(args: &PathArguments) -> Option<&GenericArgument> {
+    match args {
+        PathArguments::AngleBracketed(args) => args.args.first(),
+        _ => None,
+    }
+}
+
+fn get_generic_type(arg: &GenericArgument) -> Option<&Type> {
+    match arg {
+        GenericArgument::Type(ty) => Some(ty),
+        _ => None,
     }
 }
